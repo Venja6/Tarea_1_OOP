fb188d6f682ed48f0df0677645fb3f679588e728
Benjamin <benjamidiaz2023@udec.cl>
2025-04-29 16:46:01 -0400

    tercer cambio

--- prueba1/src/main/java/org/example/Main.java
+++ prueba1/src/main/java/org/example/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("hola2");
         System.out.println("cambio en nueva");
         System.out.println("segundo cambio en nueva");
+        System.out.println("tercer cambio en nueva");
     }
 }
