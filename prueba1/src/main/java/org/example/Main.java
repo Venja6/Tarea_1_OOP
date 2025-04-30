ffeb4de3b7a97a7f386cf54b7a072fab7e983050
Benjamin <benjamidiaz2023@udec.cl>
2025-04-30 00:18:41 -0400

    test

--- prueba1/src/main/java/org/example/Main.java
+++ prueba1/src/main/java/org/example/Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println("cambio en nueva");
         System.out.println("segundo cambio en nueva");
         System.out.println("tercer cambio en nueva");
+        System.out.println("cambio en branch");
     }
 }
